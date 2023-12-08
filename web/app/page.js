9dea9a057835ac3f1fc37dd62614244b6a7b171f
JeanJoblin <invisibleorbs@gmail.com>
2023-12-07 20:47:00 -0500

    address comments
    fix sort bug

--- web/app/page.js
+++ web/app/page.js
@@ -1,46 +1,40 @@
 'use client'
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useLayoutEffect } from 'react';
 import Allergen from './components/allergen'
 
 let allergenNames = [
-  'Milk',
-  'Eggs',
-  'Nuts',
-  'Fish',
-  'Wheat',
-  'Soy',
-  'Sesame'
+  {name: 'Milk', time: 1},
+  {name: 'Eggs', time: 33},
+  {name: 'Nuts', time: 5},
+  {name: 'Fish', time: 67},
+  {name: 'Wheat', time: 34},
+  {name: 'Soy', time: 35},
+  {name: 'Sesame', time: 3},
 ]
 
 export default function Home() {
-  const [allergens, setAllergens] = useState(allergenNames.map(allergenTimeMapCallback));
-
-  function allergenTimeMapCallback(name, ind) {
-    return {name, time: ind}
-  }
-
-  useEffect(() => {
-    setAllergens(a => a.sort(allergenTimeSortCallback))
-  }, []);
-
-  function allergenTimeSortCallback(a, b) {
-    if(a.time > b.time) {
-      return 1
-    }
-    if(a.time < b.time) {
-      return -1
-    }
-    return 0
-  }
+  const [allergens, setAllergens] = useState(allergenNames.sort(allergenTimeSortCallback));
 
   return (
     <main>
       <div className="flex flex-col items-center w-[100vw] h-[100vh]">
         <h1 className=" text-6xl animate-pulse font-bold pb-10">Food Allergen Tracker</h1>
-        <div>
-          {allergens.map(allergen => <Allergen key={allergen.name} props={{name: allergen.name, time: allergen.time}}/>)}
-        </div>
+        <ul>
+          {allergens.map(({name, time}) => {
+            return (
+            <li key={name}>
+
+              <Allergen
+                props={ {name, time} }/>
+
+            </li>
+          )})}
+        </ul>
       </div>
     </main>
   )
+}
+
+function allergenTimeSortCallback(a, b) {
+  return b.time - a.time
 }

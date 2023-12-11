cf4dcf52d01371097dd0a8756b6947b01eda75c6
JeanJoblin <invisibleorbs@gmail.com>
2023-12-11 11:46:04 -0500

    make times persist
    remove resetter component

--- web/app/page.js
+++ web/app/page.js
@@ -1,40 +1,86 @@
-'use client'
-import { useEffect, useState, useLayoutEffect } from 'react';
-import Allergen from './components/allergen'
+"use client";
+import { useEffect, useState } from "react";
+import Since from "./components/since";
 
 let allergenNames = [
-  {name: 'Milk', time: 1},
-  {name: 'Eggs', time: 33},
-  {name: 'Nuts', time: 5},
-  {name: 'Fish', time: 67},
-  {name: 'Wheat', time: 34},
-  {name: 'Soy', time: 35},
-  {name: 'Sesame', time: 3},
-]
+  { name: "Milk", time: null },
+  { name: "Eggs", time: null },
+  { name: "Nuts", time: null },
+  { name: "Fish", time: null },
+  { name: "Wheat", time: null },
+  { name: "Soy", time: null },
+  { name: "Sesame", time: null },
+];
 
 export default function Home() {
-  const [allergens, setAllergens] = useState(allergenNames.sort(allergenTimeSortCallback));
+  const [allergens, setAllergens] = useState(
+    allergenNames.sort(allergenTimeSortCallback)
+  );
+
+  useEffect(() => {
+    const newTimes = loadTimesFromLocalStorage();
+    setAllergens(newTimes.sort(allergenTimeSortCallback));
+  }, []);
+
+  function updateTime(targetAllergen) {
+    setAllergens(
+      allergens.map((allergen) => {
+        if (allergen.name === targetAllergen) {
+          const time = new Date();
+          window.localStorage.setItem(allergen.name, time);
+          return { name: targetAllergen, time };
+        } else {
+          return allergen;
+        }
+      })
+    );
+    setAllergens((a) => a.sort(allergenTimeSortCallback));
+  }
 
   return (
     <main>
       <div className="flex flex-col items-center w-[100vw] h-[100vh]">
-        <h1 className=" text-6xl animate-pulse font-bold pb-10">Food Allergen Tracker</h1>
+        <h1 className=" text-6xl animate-pulse font-bold pb-10">
+          Food Allergen Tracker
+        </h1>
         <ul>
-          {allergens.map(({name, time}) => {
+          {allergens.map(({ name, time }) => {
             return (
-            <li key={name}>
-
-              <Allergen
-                props={ {name, time} }/>
-
-            </li>
-          )})}
+              <li key={name}>
+                <p className="font-bold text-lg">{name}</p>
+                <Since props={time} />
+                <button
+                  onClick={() => {
+                    updateTime(name);
+                  }}
+                >
+                  Fed
+                </button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </main>
-  )
+  );
 }
 
 function allergenTimeSortCallback(a, b) {
-  return b.time - a.time
+  if (a.time && b.time) {
+    return a.time - b.time;
+  }
+  if (!a.time) {
+    return 1;
+  } else {
+    return -1;
+  }
+}
+
+function loadTimesFromLocalStorage() {
+  const newTimes = allergenNames.map(({ name }) => {
+    const storedTime = window.localStorage.getItem(name);
+    const time = storedTime ? new Date(storedTime) : null;
+    return { name, time };
+  });
+  return newTimes;
 }

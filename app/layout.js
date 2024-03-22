c5688b1058683f52730829fe635eb029daa29868
JeanJoblin <invisibleorbs@gmail.com>
2024-03-21 21:08:25 -0400

    remove test

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Allergen Tracker test',
+  title: 'Allergen Tracker',
   description: 'Track how long it has been since your baby had these common allergens',
 }
 
